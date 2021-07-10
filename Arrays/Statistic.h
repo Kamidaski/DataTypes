fc8f2a1ba906a1c555ed66f809abd2db539a5d1e
Cere_wb33@itstepru.onmicrosoft.com <Cere_wb33@itstepru.onmicrosoft.com>
2021-07-10 18:40:07 +0800

    Separation DONE

---
+++ Arrays/Statistic.h
@@ -0,0 +1,26 @@
+#pragma once
+#include"stdafx.h"
+
+int Sum(int arr[], const int n);
+double Sum(double arr[], const int n);
+float Sum(float arr[], const int n);
+char Sum(char arr[], const int n);
+short Sum(short arr[], const int n);
+
+double Avg(int arr[], const int n);
+double Avg(double arr[], const int n);
+double Avg(float arr[], const int n);
+double Avg(char arr[], const int n);
+double Avg(short arr[], const int n);
+
+int minValueIn(int arr[], const int n);
+double minValueIn(double arr[], const int n);
+float minValueIn(float arr[], const int n);
+char minValueIn(char arr[], const int n);
+short minValueIn(short arr[], const int n);
+
+int maxValueIn(int arr[], const int n);
+double maxValueIn(double arr[], const int n);
+float maxValueIn(float arr[], const int n);
+char maxValueIn(char arr[], const int n);
+short maxValueIn(short arr[], const int n);
